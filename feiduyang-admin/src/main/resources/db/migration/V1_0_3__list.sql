d93c53d364770501dc9d14325d73035742ae60a0
raozhidan@lixin360.com <12345678>
2018-06-19 16:56:07 +0800

    更新配置信息

--- feiduyang-admin/src/main/resources/db/migration/V1_0_3__list.sql
+++ feiduyang-admin/src/main/resources/db/migration/V1_0_3__list.sql
@@ -1,18 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema ffast
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema ffast
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ffast` DEFAULT CHARACTER SET latin1 ;
-USE `ffast` ;
 
 -- -----------------------------------------------------
 -- Table `ffast`.`t_flyway_schema`
